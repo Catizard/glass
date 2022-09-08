5af322d7739b89be66c7ab998036b474fbb33c27
Catizard <1185032459@qq.com>
2022-09-08 12:59:32 +0800

    merged network

--- src/main/java/com/catizard/glass/network/server/DefaultRunnableNettyServer.java
+++ src/main/java/com/catizard/glass/network/server/DefaultRunnableNettyServer.java
@@ -4,23 +4,8 @@ import com.catizard.glass.network.server.abstracts.NettyServer;
 import com.catizard.glass.utils.InetAddress;
 
 public class DefaultRunnableNettyServer extends NettyServer implements Runnable {
-    private Thread thread;
     @Override
     public void run() {
         setupServer();
-    }
-
-    @Override
-    public void listenTo(int port) {
-        super.listenTo(port);
-        thread = new Thread(this);
-        thread.start();
-    }
-
-    @Override
-    public void listenTo(InetAddress ip) {
-        super.listenTo(ip);
-        thread = new Thread(this);
-        thread.start();
     }
 }
